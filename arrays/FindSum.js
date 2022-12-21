4bf84bc9378fc4867eab28fe29b2986554f29621
Manoj H N <manojhn036@gmail.com>
2022-12-21 10:36:12 +0530

    resolved output issue

--- arrays/FindSum.js
+++ arrays/FindSum.js
@@ -4,7 +4,7 @@ function findSum(arr, value) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[i] + arr[j] == value) {
-        return arr[i], arr[j];
+        return [arr[i], arr[j]];
       }
     }
   }
